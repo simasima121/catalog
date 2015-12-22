0a0af234b4053f994f153cd3fea33a4aa6eb9571
Sim Virdi <simeransvirdi@gmail.com>
2015-12-22 20:09:04 +0000

    Creating initial files

---
+++ database_setup.py
@@ -0,0 +1,63 @@
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+class User(Base):
+	__tablename__ = 'user'
+
+   id = Column(Integer, primary_key=True)
+   name = Column(String(250), nullable=False)
+   email = Column(String(250), nullable=False)
+   picture = Column(String(250))
+
+class Section(Base):
+    __tablename__ = 'section'
+   
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+    sectionItems = relationship("SectionItem", cascade="all, delete-orphan")
+
+    @property
+    def serialize(self):
+       """Return object data in easily serializeable format"""
+       return {
+           'name'         : self.name,
+           'id'           : self.id,
+           'user_id'      : self.user_id
+       }
+
+
+class SectionItem(Base):
+    __tablename__ = 'section_item'
+
+    name = Column(String(80), nullable=False)
+    id = Column(Integer, primary_key=True)
+    description = Column(String(250))
+    category = Column(String(250))
+    restaurant_id = Column(Integer, ForeignKey('restaurant.id'))
+    restaurant = relationship(Restaurant)
+
+    restaurant_id = Column(Integer,ForeignKey('restaurant.id'))
+    restaurant = relationship(Restaurant)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+    @property
+    def serialize(self):
+       """Return object data in easily serializeable format"""
+       return {
+           'name'         : self.name,
+           'description'         : self.description,
+           'id'         : self.id,
+           'price'         : self.price,
+           'course'         : self.course,
+       }
+
+engine = create_engine('sqlite:///restaurantmenu.db')
+
+Base.metadata.create_all(engine)
