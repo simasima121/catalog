8743d2ba69122c794be87ecb356bbcd8c3a38eda
Sim Virdi <simeransvirdi@gmail.com>
2016-01-06 17:06:19 +0000

    Created routing for website

--- application.py
+++ application.py
@@ -1,11 +1,64 @@
-from flask import Flask 
+from flask import Flask, render_template, request, redirect, jsonify, url_for
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, User, Catalog, Category, CategoryItem
+
 app = Flask(__name__)
 
+#engine = create_engine('sqlite:///catalog.db')
+#Base.metadata.bind = engine
+
+#DBSession = sessionmaker(bind=engine)
+#session = DBSession()
+
 @app.route('/')
-@app.route('/hello/')
-def HelloWorld():
-	return "Hello World!!!!"
+@app.route('/catalog/')
+def showCatalog():
+	#return "This page will show the Catalog"
+	return render_template('restaurants.html', restaurants=restaurants)
+
+@app.route('/catalog/new/')
+def newCategory():
+	#return "New Category Page"
+	return render_template('restaurants.html', restaurants=restaurants)
+
+@app.route('/catalog/<string:category_name>/')
+@app.route('/catalog/<string:category_name>/items/')
+def showCategory(category_name):
+	#return "This page will show the Category Items of: %s" % category_name
+	return render_template('restaurants.html', restaurants=restaurants)
+
+@app.route('/catalog/<string:category_name>/edit/')
+def editCategory(category_name):
+	#return "Edit Category name of: %s" % category_name
+	return render_template('restaurants.html', restaurants=restaurants)
+
+@app.route('/catalog/<string:category_name>/delete/')
+def deleteCategory(category_name):
+	#return "Delete Category of: %s" % category_name
+	return render_template('restaurants.html', restaurants=restaurants)
+
+@app.route('/catalog/<string:category_name>/new/')
+@app.route('/catalog/<string:category_name>/items/new/')
+def newCategoryItem(category_name):
+	#return "New Category Item of: %s" % category_name
+	return render_template('restaurants.html', restaurants=restaurants)
+
+@app.route('/catalog/<string:category_name>/<string:item_name>/')
+def showCategoryItem(category_name, item_name):
+	#return "Page shows description of item: %s" % item_name
+	return render_template('restaurants.html', restaurants=restaurants)
+
+@app.route('/catalog/<string:category_name>/<string:item_name>/edit/')
+def editCategoryItem(category_name, item_name):
+	#return "Edit category item of: %s" % item_name
+	return render_template('restaurants.html', restaurants=restaurants)
+
+@app.route('/catalog/<string:category_name>/<string:item_name>/delete/')
+def deleteCategoryItem(category_name, item_name):
+	#return "delete category item of: %s" % item_name
+	return render_template('restaurants.html', restaurants=restaurants)
 
 if __name__ == '__main__':
-	app.debug = True
-	app.run(host = '0.0.0.0', port = 8000)
+	 app.debug = True
+	 app.run(host='0.0.0.0', port = 8000)
--- database_setup.py
+++ database_setup.py
@@ -10,52 +10,62 @@ Base = declarative_base()
 class User(Base):
 	__tablename__ = 'user'
 
-   id = Column(Integer, primary_key=True)
-   name = Column(String(250), nullable=False)
-   email = Column(String(250), nullable=False)
-   picture = Column(String(250))
+	id = Column(Integer, primary_key=True)
+	name = Column(String(250), nullable=False)
+	email = Column(String(250), nullable=False)
+	picture = Column(String(250))
 
-class Section(Base):
-    __tablename__ = 'section'
-   
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
-    user_id = Column(Integer, ForeignKey('user.id'))
-    user = relationship(User)
-    sectionItems = relationship("SectionItem", cascade="all, delete-orphan")
+class Catalog(Base):
+	__tablename__ = 'catalog'
+	
+	id = Column(Integer, primary_key=True)
+	name = Column(String(250), nullable=False)
+	user_id = Column(Integer, ForeignKey('user.id'))
+	user = relationship(User)
 
-    @property
-    def serialize(self):
-       """Return object data in easily serializeable format"""
-       return {
-           'name'         : self.name,
-           'id'           : self.id,
-           'user_id'      : self.user_id
-       }
+	@property
+	def serialize(self):
+		"""Return object data in easily serializeable format"""
+		return {
+			'name'         : self.name,
+			'id'           : self.id,
+			'user_id'      : self.user_id
+		}
 
+class Category(Base):
+	__tablename__ = 'category'
 
-class SectionItem(Base):
-    __tablename__ = 'section_item'
+	name = Column(String(80), nullable=False)
+	id = Column(Integer, primary_key=True)
+	description = Column(String(250))
+	catalog_id = Column(Integer, ForeignKey('catalog.id'))
+	catalog = relationship(Catalog)
+	user_id = Column(Integer, ForeignKey('user.id'))
+	user = relationship(User)
 
-    name = Column(String(80), nullable=False)
-    id = Column(Integer, primary_key=True)
-    description = Column(String(250))
-    category = Column(String(250))
-    section_id = Column(Integer, ForeignKey('section.id'))
-    section = relationship(Section)
-    user_id = Column(Integer, ForeignKey('user.id'))
-    user = relationship(User)
+	@property
+	def serialize(self):
+		"""Return object data in easily serializeable format"""
+		return {
+			'name'         	: self.name,
+			'description'    : self.description,
+			'id'          	  : self.id,
+			'category'       : self.category,
+		}
 
-    @property
-    def serialize(self):
-       """Return object data in easily serializeable format"""
-       return {
-           'name'         	: self.name,
-           'description'   : self.description,
-           'id'         	: self.id,
-           'category'      : self.category,
-       }
+class CategoryItem(Base):
+	__tablename__ = 'category_item'
 
-engine = create_engine('sqlite:///restaurantmenu.db')
+	name = Column(String(80), nullable=False)
+	id = Column(Integer, primary_key=True)
+	description = Column(String(250))
+	category_id = Column(Integer, ForeignKey('category.id'))
+	category = relationship(Category)
+	catalog_id = Column(Integer, ForeignKey('catalog.id'))
+	catalog = relationship(Catalog)
+	user_id = Column(Integer, ForeignKey('user.id'))
+	user = relationship(User)
+
+engine = create_engine('sqlite:///catalog.db')
 
 Base.metadata.create_all(engine)
