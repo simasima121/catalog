6505f5284ad165e6d26c623d03e77c63354c3001
Sim Virdi <simeransvirdi@gmail.com>
2016-01-09 19:52:33 +0000

    added CRUD functionality to the website

--- application.py
+++ application.py
@@ -1,80 +1,125 @@
 from flask import Flask, render_template, request, redirect, jsonify, url_for
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from database_setup import Base, User, Category, CategoryItem
-
-#Fake Categories
-category = {'name':'Soccer', 'description':'Football match', 'id': '1'}
-categorys = [ {'name':'Soccer', 'description':'Football match','id':'1'}, {'name':'Basketball', 'description':'Basketball match','id':'2'},{'name':'Baseball', 'description':'Baseball match','id':'3'},{'name':'Frisbee', 'description':'Frisbee match','id':'4'} ]
-
-#Fake Items
-item =  {'name':'Arsenal', 'description':'Arsenal team'}
-items = [ {'name':'Arsenal', 'description':'Arsenal team', 'id': '1'}, {'name':'Man United', 'description':'Man United team','id':'2'},{'name':'Chelsea', 'description':'Chelsea team','id':'3'},{'name':'West Ham', 'description':'West Ham team','id':'4'} ]
+from database_setup import Base, Category, CategoryItem
 
 app = Flask(__name__)
 
-#engine = create_engine('sqlite:///catalog.db')
-#Base.metadata.bind = engine
+engine = create_engine('sqlite:///catalog.db')
+Base.metadata.bind = engine
 
-#DBSession = sessionmaker(bind=engine)
-#session = DBSession()
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
 
 #Show Catalog
 @app.route('/')
 @app.route('/catalog/')
 def showCatalog():
-	#return "This page will show the Catalog"
+	categorys = session.query(Category).all()
 	return render_template('catalog.html', categorys = categorys)
 
-#Create new Category
-@app.route('/catalog/new/')
-def newCategory():
-	#return "New Category Page"
-	return render_template('newcategory.html')
 
-#Look at items in category
-@app.route('/catalog/<string:category_name>/')
-@app.route('/catalog/<string:category_name>/items/')
+#Create new Category
+@app.route('/catalog/new/', methods=['GET', 'POST'])
+def newCategory():
+	if request.method == 'POST':
+		  newCategory = Category(name=request.form['name'], description=request.form['description'])
+		  session.add(newCategory)
+		  session.commit()
+		  return redirect(url_for('showCatalog'))
+	else:
+		  return render_template('newcategory.html')	
+
+
+#Show category items
+@app.route('/catalog/<category_name>/')
+@app.route('/catalog/<category_name>/items/')
 def showCategory(category_name):
-	#return "This page will show the Category: %s" % category_name
-	return render_template('category.html', category = category, items=items)
+	category = session.query(Category).filter_by(name=category_name).one()
+	category_id = category.id
+	items= session.query(CategoryItem).filter_by(category_id = category_id).all()
+	return render_template('category.html', category = category, items=items)	
+
 
 #Edit category name
-@app.route('/catalog/<string:category_name>/edit/')
+@app.route('/catalog/<category_name>/edit/', methods=['GET', 'POST'])
 def editCategory(category_name):
-	#return "Edit Category: %s" % category_name
-	return render_template('editcategory.html', category = category)
+	editedCategory = session.query(Category).filter_by(name=category_name).one()
+	if request.method == 'POST':
+		if request.form['name']:
+			editedCategory.name = request.form['name']
+		if request.form['description']:
+			editedCategory.description = request.form['description']
+		session.add(editedCategory)
+		session.commit()
+		return redirect(url_for('showCatalog'))
+	else:
+		return render_template('editcategory.html', category = editedCategory)	
+
 
 #Delete category
-@app.route('/catalog/<string:category_name>/delete/')
+@app.route('/catalog/<category_name>/delete/', methods=['GET', 'POST'])
 def deleteCategory(category_name):
-	#return "Delete Category: %s" % category_name
-	return render_template('deletecategory.html', category = category)
+	categoryToDelete = session.query(Category).filter_by(name=category_name).one()
+	if request.method == 'POST':
+		session.delete(categoryToDelete)
+		session.commit()
+		return redirect(url_for('showCatalog'))
+	else:
+		return render_template('deletecategory.html', category = categoryToDelete)	
+
 
 #Create new category item
-@app.route('/catalog/<string:category_name>/new/')
-@app.route('/catalog/<string:category_name>/items/new/')
+@app.route('/catalog/<category_name>/new/', methods=['GET', 'POST'])
+@app.route('/catalog/<category_name>/items/new/', methods=['GET', 'POST'])
 def newCategoryItem(category_name):
-	#return "New Category Item of: %s" % category_name
-	return render_template('newitem.html', category = category)
+	category = session.query(Category).filter_by(name=category_name).one()
+	category_id = category.id
+	if request.method == 'POST':
+		newItem = CategoryItem(name=request.form['name'], description=request.form['description'],
+									  category_id=category_id)
+		session.add(newItem)
+		session.commit()
+
+		return redirect(url_for('showCategory', category_name=category_name))
+	else:
+		return render_template('newitem.html', category_name = category_name)	
+
 
 #Look at category item
-@app.route('/catalog/<string:category_name>/<string:item_name>/')
+@app.route('/catalog/<category_name>/<item_name>/')
 def showCategoryItem(category_name, item_name):
-	#return "Page shows description of item: %s" % item_name
-	return render_template('item.html',category_name=category_name, item_name=item_name, item=item, category=category)
+	category = session.query(Category).filter_by(name=category_name).one()
+	item = session.query(CategoryItem).filter_by(name=item_name).one()
+	return render_template('item.html', item=item, category=category)
+
 
 #Edit category item
-@app.route('/catalog/<string:category_name>/<string:item_name>/edit/')
+@app.route('/catalog/<category_name>/<item_name>/edit/', methods=['GET', 'POST'])
 def editCategoryItem(category_name, item_name):
-	#return "Edit category item of: %s" % item_name
-	return render_template('edititem.html', category_name=category_name, item_name=item_name, item=item,category=category)
+	editedItem = session.query(CategoryItem).filter_by(name=item_name).one()
+	if request.method == 'POST':
+		if request.form['name']:
+			editedItem.name = request.form['name']
+		if request.form['description']:
+			editedItem.description = request.form['description']
+		session.add(editedItem)
+		session.commit()
+		return redirect(url_for('showCategory', category_name=category_name))
+	else:
+		return render_template('edititem.html', category_name=category_name, item_name=item_name, item=editedItem)
+
 
 #Delete category item
-@app.route('/catalog/<string:category_name>/<string:item_name>/delete/')
+@app.route('/catalog/<category_name>/<item_name>/delete/', methods=['GET', 'POST'])
 def deleteCategoryItem(category_name, item_name):
-	#return "delete category item of: %s" % item_name
-	return render_template('deleteitem.html', category_name=category_name, item_name=item_name, item=item,category=category)
+	itemToDelete = session.query(CategoryItem).filter_by(name=item_name).one()
+	if request.method == 'POST':
+		session.delete(itemToDelete)
+		session.commit()
+		return redirect(url_for('showCategory', category_name=category_name))
+	else:
+		return render_template('deleteitem.html', category_name=category_name, item_name=item_name, item=itemToDelete)
 
 if __name__ == '__main__':
 	 app.debug = True
--- database_setup.py
+++ database_setup.py
@@ -7,64 +7,43 @@ from sqlalchemy import create_engine
 
 Base = declarative_base()
 
-class User(Base):
-	__tablename__ = 'user'
+#class User(Base):
+	#__tablename__ = 'user'
 
-	id = Column(Integer, primary_key=True)
-	name = Column(String(250), nullable=False)
-	email = Column(String(250), nullable=False)
-	picture = Column(String(250))
-
-class Catalog(Base):
-	__tablename__ = 'catalog'
-	
-	id = Column(Integer, primary_key=True)
-	name = Column(String(250), nullable=False)
-	user_id = Column(Integer, ForeignKey('user.id'))
-	user = relationship(User)
-
-	@property
-	def serialize(self):
-		"""Return object data in easily serializeable format"""
-		return {
-			'name'         : self.name,
-			'id'           : self.id,
-			'user_id'      : self.user_id
-		}
+	#id = Column(Integer, primary_key=True)
+	#name = Column(String(250), nullable=False)
+	#email = Column(String(250), nullable=False)
+	#picture = Column(String(250))
 
 class Category(Base):
 	__tablename__ = 'category'
 
 	name = Column(String(80), nullable=False)
 	id = Column(Integer, primary_key=True)
-	description = Column(String(250))
-	catalog_id = Column(Integer, ForeignKey('catalog.id'))
-	catalog = relationship(Catalog)
-	user_id = Column(Integer, ForeignKey('user.id'))
-	user = relationship(User)
+	description = Column(String(250), nullable=False)
 
-	@property
-	def serialize(self):
-		"""Return object data in easily serializeable format"""
-		return {
-			'name'         	: self.name,
-			'description'    : self.description,
-			'id'          	  : self.id,
-			'category'       : self.category,
-		}
+	#user_id = Column(Integer, ForeignKey('user.id'))
+	#user = relationship(User)
+
+	#@property
+	#def serialize(self):
+	#	"""Return object data in easily serializeable format"""
+#		return {
+#			'name'         	: self.name,
+#			'description'    : self.description,
+#			'id'          	  : self.id,
+	#	}
 
 class CategoryItem(Base):
 	__tablename__ = 'category_item'
 
 	name = Column(String(80), nullable=False)
 	id = Column(Integer, primary_key=True)
-	description = Column(String(250))
+	description = Column(String(250),nullable=False)
 	category_id = Column(Integer, ForeignKey('category.id'))
 	category = relationship(Category)
-	catalog_id = Column(Integer, ForeignKey('catalog.id'))
-	catalog = relationship(Catalog)
-	user_id = Column(Integer, ForeignKey('user.id'))
-	user = relationship(User)
+	#user_id = Column(Integer, ForeignKey('user.id'))
+	#user = relationship(User)
 
 engine = create_engine('sqlite:///catalog.db')
 
